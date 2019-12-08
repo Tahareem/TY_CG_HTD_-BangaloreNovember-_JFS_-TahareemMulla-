672da4a64fed810614195f8f593308c255fb5a3f
Tahareem <tahareem55@gmail.com>
2019-12-08 21:30:25 +0530

    arrays in java

---
+++ arrays/ArrayChar.java
@@ -0,0 +1,17 @@
+
+public class ArrayChar 
+{
+	public static void main(String[] args) {
+		char c1[]=new char[5];
+		c1[0]='t';
+		c1[1]='a';
+		c1[2]='n';
+		c1[3]=10;
+		c1[4]='u';
+		
+		for(int i=0;i<c1.length;i++)
+		{
+			System.out.println(c1[i]);
+		}
+	}
+}
---
+++ arrays/ArrayDouble.java
@@ -0,0 +1,16 @@
+
+public class ArrayDouble 
+{
+	public static void main(String[] args) {
+		double[] d1=new double[4];
+		d1[0]=11.5;
+		d1[1]=4;
+		d1[2]=33;
+		
+		for(int i=0;i<d1.length;i++)
+		{
+			System.out.println(d1[i]);
+		}
+		
+	}
+}
